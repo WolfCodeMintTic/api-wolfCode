bba129d0e98d8e45ade03abe1e8e66fd033b5dff
Franklin <franklinq8@gmail.com>
2021-10-21 13:54:53 -0500

    agregando jwt

--- server.js
+++ server.js
@@ -5,11 +5,25 @@ var {conectarBD, getDB} = require('./DB/db.js');
 var rutasProductos = require('./views/productos/rutas.js');
 var rutasUsuarios = require('./views/usuarios/rutas.js');
 var rutasVentas = require('./views/ventas/rutas.js');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 //import express from 'express';
 
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://wolfcode-mintic.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'api-wolfcode-auth',
+    issuer: 'https://wolfcode-mintic.us.auth0.com/',
+    algorithms: ['RS256']
+});
 
 const app = Express();
 
+app.use(jwtCheck);
 app.use(Express.json());
 app.use(Cors());
 app.use(rutasProductos);
